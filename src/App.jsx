630690797acdca9d69bec1ce9518409bd0eac312
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2024-11-25 09:00:50 +0300

    Update included:   - Setup BrowserROuter to be able to use the package globally   - Setup the default routes for Home, about, contact, gallery and blog

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import './index.css'
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Gallery from './Pages/Gallery';
+import Contact from './Pages/Contact';
+import Blog from './Pages/Blog';
 
 function App() {
   return (
-    <div className="container mx-auto bg-theme-white p-5p tablet:p-8p desktop:p-10p">
-      <h1 className="text-2xl font-red-hat-display">Welcome</h1>
-      <h2 className="text-xl">Section Title</h2>
-      <h3 className="text-lg">Subheading</h3>
-      <h4 className="text-base">Minor Heading</h4>
-      <p>This is a paragraph with custom text styling.</p>
-      <ul>
-        <li>First bullet point</li>
-        <li>Second bullet point</li>
-      </ul>
-      <ol>
-        <li>First numbered point</li>
-        <li>Second numbered point</li>
-      </ol>
-      <a href="#" className="hover:bg-theme-brown hover:text-theme-white">
-        Read More
-      </a>
-    </div>
+    <Routes>
+      <Route path="/" element={ <Home/> } />
+      <Route path="about" element={ <About/> } />
+      <Route path="gallery" element={ <Gallery/> } />
+      <Route path="contact" element={ <Contact/> } />
+      <Route path="blog" element={ <Blog/> } />
+    </Routes>
   );
 }
 
