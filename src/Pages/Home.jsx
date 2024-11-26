51dd3b26caf00fb9623557df1d6a3c63a8cfdacb
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2024-11-26 15:46:06 +0300

    Add the IntroSection abd ServiceCardGrid components to the Home component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from '../Components/Navbar/Navbar'
 import Slider from '../Components/Navbar/Slider'
+import IntroSection from '../Sections/Home/IntroSection'
+import ServiceCardGrid from '../Sections/Home/ServiceCardGrid'
 
 function Home() {
     return (
         <div>
           <Navbar />
           <Slider />
+          <IntroSection />
+          <ServiceCardGrid />
         </div>
     )
 }
