d3d8757323bf568985d5b2225d4542ad9083c912
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2024-12-28 17:11:46 +0300

    Created sample data to use for the gallery page.I will update with the respective images

---
+++ src/data/galleryData.js
@@ -0,0 +1,106 @@
+const galleryData = [
+  { id: 1, 
+    image: "", 
+    imageType: "Corporates" 
+  },
+  { id: 2, 
+    image: "", 
+    imageType: "Lounges & Cocktail Furniture" 
+  },
+  { id: 3, 
+    image: "", 
+    imageType: "Private Parties" 
+  },
+  { id: 4, 
+    image: "", 
+    imageType: "Props & Decor" 
+  },
+  { id: 5, 
+    image: "", 
+    imageType: "Sustainable Furniture" 
+  },
+  { id: 6, 
+    image: "", 
+    imageType: "Weddings" 
+  },
+  { id: 7, 
+    image: "", 
+    imageType: "Rustic Furniture" 
+  },
+  { id: 8, 
+    image: "", 
+    imageType: "Lounges & Cocktail Furniture" 
+  },
+  { id: 9, 
+    image: "", 
+    imageType: "Private Parties" 
+  },
+  { id: 10, 
+    image: "", 
+    imageType: "Props & Decor" 
+  },
+  { id: 11, 
+    image: "", 
+    imageType: "Corporates" 
+  },
+  { id: 12, 
+    image: "", 
+    imageType: "Sustainable Furniture" 
+  },
+  { id: 13, 
+    image: "", 
+    imageType: "Weddings" 
+  },
+  { id: 14, 
+    image: "", 
+    imageType: "Rustic Furniture" 
+  },
+  { id: 15, 
+    image: "", 
+    imageType: "Lounges & Cocktail Furniture" 
+  },
+  { id: 16, 
+    image: "", 
+    imageType: "Private Parties" 
+  },
+  { id: 17, 
+    image: "", 
+    imageType: "Props & Decor" 
+  },
+  { id: 18, 
+    image: "", 
+    imageType: "Corporates" 
+  },
+  { id: 19, 
+    image: "", 
+    imageType: "Sustainable Furniture" 
+  },
+  { id: 20, 
+    image: "", 
+    imageType: "Weddings" 
+  },
+  { id: 21, 
+    image: "", 
+    imageType: "Rustic Furniture" 
+  },
+  { id: 22, 
+    image: "", 
+    imageType: "Lounges & Cocktail Furniture" 
+  },
+  { id: 23, 
+    image: "", 
+    imageType: "Private Parties" 
+  },
+  { id: 24, 
+    image: "", 
+    imageType: "Props & Decor" 
+  },
+  { id: 25, 
+    image: "", 
+    imageType: "Corporates" 
+  },
+  { id: 26, 
+    image: "", 
+    imageType: "Sustainable Furniture" 
+  },
+];
