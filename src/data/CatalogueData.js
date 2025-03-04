4234c06cc74bbae1a8fefcb673feb617d7716d10
Nemwel-Boniface <nemwelboniface@outlook.com>
2025-03-04 09:50:14 +0300

    Add thehe remaining categories images

--- src/data/CatalogueData.js
+++ src/data/CatalogueData.js
@@ -9,25 +9,28 @@ const catalogData = [
       `${baseCatalogeURL}catalogue1-2.jpeg`,
       `${baseCatalogeURL}catalogue1-3.jpeg`,
       `${baseCatalogeURL}catalogue1-4.jpeg`,
-      `${baseCatalogeURL}catalogue1-5.jpeg`
+      `${baseCatalogeURL}catalogue1-5.jpeg`,
+      `${baseCatalogeURL}catalogue2-1.png`,
     ],
   },
   {
     catalogue_name: "Tables",
-    main_image: "https://www.figma.com/file/zeQHGAFT609JaA0UaV9BYn/image/3d0f911ccbb759f2ae8daedd54fcb364c26b9f04",
+    main_image: `${baseCatalogeURL}catalogue2-main.jpeg`,
     other_images: [
-      "https://www.figma.com/file/zeQHGAFT609JaA0UaV9BYn/image/04e1013804e329febaaf88ea0af3d5341e112bc7",
-      "https://www.figma.com/file/zeQHGAFT609JaA0UaV9BYn/image/d40d41916e9356b5d49b5526d3438a020f4a2ed9",
-      "https://www.figma.com/file/zeQHGAFT609JaA0UaV9BYn/image/be69558be17a40d99028ed26c2a1dfdad9922ab3",
+      `${baseCatalogeURL}catalogue2-1.png`,
+      `${baseCatalogeURL}catalogue2-2.png`,
+      `${baseCatalogeURL}catalogue2-3.jpeg`,
+      `${baseCatalogeURL}catalogue2-4.jpeg`,
     ],
   },
   {
     catalogue_name: "Tables",
-    main_image: "https://www.figma.com/file/zeQHGAFT609JaA0UaV9BYn/image/be69558be17a40d99028ed26c2a1dfdad9922ab3",
+    main_image: `${baseCatalogeURL}catalogue3-main.jpeg`,
     other_images: [
-      "https://www.figma.com/file/zeQHGAFT609JaA0UaV9BYn/image/a2a14be55d822395e6086e05012f1c942b3dd64c",
-      "https://www.figma.com/file/zeQHGAFT609JaA0UaV9BYn/image/d9038b3659f7ed0c82e06e2260655a8e14ec4043",
-      "https://www.figma.com/file/zeQHGAFT609JaA0UaV9BYn/image/191fc8fa5c4146e280d2078bcb8330a14cccd64d",
+      `${baseCatalogeURL}catalogue3-1.jpeg`,
+      `${baseCatalogeURL}catalogue3-2.jpeg`,
+      `${baseCatalogeURL}catalogue3-3.jpeg`,
+      `${baseCatalogeURL}catalogue3-4.jpeg`,
     ],
   },
 ];
