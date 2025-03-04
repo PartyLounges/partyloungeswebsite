90a3b78ea4ce0005102bb4991110094da7d1e010
Nemwel-Boniface <nemwelboniface@outlook.com>
2025-03-04 09:40:53 +0300

    Add first batch of catalogue images

--- src/data/CatalogueData.js
+++ src/data/CatalogueData.js
@@ -1,11 +1,15 @@
+const baseCatalogeURL = "/assets/images/Catalogues/"
+
 const catalogData = [
   {
     catalogue_name: "Couches",
-    main_image: "https://s3-alpha-sig.figma.com/img/ebfc/237a/4798422a4d77193e4eed52264e67f26d?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=MFJMCyWgJMVdAJkD5CNcn9dKDLA~ZDBwITeZsSnWiC4rXIumDZ-fW-ugW9DsPZ2AImeSv5nKiksamjAKg6XM7GC4qVZmfLe194jrfoGpxT5dmKvD-wO2VC~BqEdWdx0WlkqZHmYXRZZbQfM0sBoV-IKOUcg1xSlb-pyei2Hwx8OJFTUE1y4UhjxxzuW0NUpU30ucQ0XQWraR6hcePdWbxmD0FM4-wznjuXIfXK4Gd8lym4SNUo1HeVFVa2ZJB1BIGVseAYrRf4KA0dQAOuCdmAp6rjENb2v-g4Z112-IQqzfDiLvY8-LMiX4rPJ9xq2t53mUJajverwWQhZxS4GhiA__",
+    main_image: `${baseCatalogeURL}catalogue1-main.jpeg`,
     other_images: [
-      "https://s3-alpha-sig.figma.com/img/b714/cc78/9ebb5130be0bc46db595d239c2222a45?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=N-kG9n03l~N~BxuFc0xVlh87PzzJaOqsSEbyXv98iIXyp62TsG0d1DvxWm1xGm5D-ZaCe8Kot0JGVRTBWA-uaMIxBCkYr5VFU2HzopKFZZqpOoIn3r6Ja5xQ2lcEptsbmdlEZViFcvu82nkjnkK91o8w3XvQIP7C1Smr1Igln1lFV-eNFplU0zoZOyW7cPXuK72sIwrDDBWFo3uGwEv8QoG3sZcAVIT29hO~LbRhwkAtlf7P1ZCdwZj2ZKi~IckMQ58kcN8fnlPqmPmbaqr5OCSa9gmCEwek8IIJ-8mk~o7YF0ETD9KtbUaw3hLM28M-Efr97IrUNBbz65teHt4QkA__",
-      "https://s3-alpha-sig.figma.com/img/9f81/c589/5c158ca020850c94e6e3c3c9b7c3d6cc?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=FXpP~zHTylNT0hnl25gecCgVqViDX8LOeVWtck~cIJ070M9f4xSS6Sv4dsjfbzERDnN35Q3hJlX4xm1ENHS~9MTfvDhoyDVb17pNfSoHSwvvCagdQEz5VbALOVWmFpfeid7P0xJicm1h5lpy24f7nLruWh7mK0nSR6p2VfzAOfscjGicq6MCUM30npdQuES9ZCFT2pVpeCXrAGosgn~MUZwxJrCSB3~PbL9dQjJ0X6VlX6tjiCVlyNG8PGYQ3oI2-yjKl8itoGg7ZNbx2ppS-GKM8T1ERO~XmxuVCEw~NLgdOwz8-TWuT1gaOVLKor61O-lKk9PxwMCgTeiZmJqHUw__",
-      "https://s3-alpha-sig.figma.com/img/ddf6/5f63/8a87d037b79124ccfe8bb6568cab3f13?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=XFGMRcTWe2YYZF70Bgfb-4Bx77xtaTq4-58psx0WlFVH~lMSACJ0RY6YydNJMQz9loYIomCw8kdH~ZRXS9Q7CWRDplqwuwRmZuXQRHEuE~~sxlkiRgdEGUYvKy3cPhYr75z7fVDLY~r8iVxB-pgnZtEvVwmwLxBjwg2oTg02P4OlapQlu2y56FDQj30VKc1o7qnXtlIZj2FqKJrjpG2fL1V4sxKSdqR1utUofF3QwKeuqZG0yztAS-CEyLUMh2YK43f-0ywC0Y3vCto4ec8hUAXY9qp2d7cEDNkPiW5AD00mLgFeKXk0bd8bSD2-VRne3uHkZNmueSPuJtqrDiDaWQ__",
+      `${baseCatalogeURL}catalogue1-1.jpeg`,
+      `${baseCatalogeURL}catalogue1-2.jpeg`,
+      `${baseCatalogeURL}catalogue1-3.jpeg`,
+      `${baseCatalogeURL}catalogue1-4.jpeg`,
+      `${baseCatalogeURL}catalogue1-5.jpeg`
     ],
   },
   {
