debc0fa912d159b39abf4c01ba0742b1ecc03acc
Nemwel-Boniface <nemwelboniface@outlook.com>
2025-03-07 22:52:32 +0300

    Update to the home intro section

--- src/Sections/Home/IntroSection.jsx
+++ src/Sections/Home/IntroSection.jsx
@@ -16,13 +16,14 @@ const IntroSection = () => {
           <div className="px-4 md:px-6 lg:px-8">
             <h2 className="text-white text-lg md:text-2xl font-proximanova-bold mb-4">
             Party Lounges: Premium Event rentals & Full-Service Event Planning & management.
-
+            </h2>
+            <p className="text-white text-sm md:text-base leading-relaxed mb-6 font-red-hat-display">
             At Party Lounges, we specialize in event management, corporate event planning, 
             and private party services. Our expert team, with an average of eight years 
             of experience, ensures seamless event coordination for corporate functions, 
             private parties, conferences, and social gatherings. From event planning to execution, 
             we handle every detail to deliver a stress-free, unforgettable experience
-            </h2>
+            </p>
             <p className="text-white text-sm md:text-base leading-relaxed mb-6 font-red-hat-display">
             Our expert team brings an average of eight years of experience
             in the convention, meeting, and event industry.Whether you're organizing a private event 
