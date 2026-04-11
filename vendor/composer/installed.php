8db4edf841ab862d2da54edbe4e6e651b7044476
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2026-04-11 12:12:56 +0300

    build(php): include vendor dependencies for cPanel deployment

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'partyloungeswebsite/contact-mailer',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '188fbfe8f5d902dd6dc37fe3d245d722cb4314f7',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'partyloungeswebsite/contact-mailer' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '188fbfe8f5d902dd6dc37fe3d245d722cb4314f7',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v7.0.2',
+            'version' => '7.0.2.0',
+            'reference' => 'ebf1655bd5b99b3f97e1a3ec0a69e5f4cd7ea088',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
